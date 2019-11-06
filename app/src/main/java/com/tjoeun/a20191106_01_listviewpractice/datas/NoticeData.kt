5fd75a91a3c0d66d66423f19fb0efa2903c1e556
holystar97 <mkrice500@gmail.com>
2019-11-06 19:56:12 +0900

    데이터 클래스 생성

---
+++ app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/datas/NoticeData.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191106_01_listviewpractice.datas
+
+class NoticeData(title:String, content:String, writeDate:String) {
+
+    var title=title
+    var content=content
+    var date=writeDate
+
+    
+
+
+}
